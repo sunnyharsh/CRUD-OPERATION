f196fdefee96148b92b589e5b04641694869e4d8
sunny kumar singh <sunnyharshsunny12345@gmail.com>
2019-02-18 15:28:48 +0530

    Add files via upload
    1. in frond and backend folder execute (nom install)
    2.run backend nodemon app.js 
    3..run frontend npm start

---
+++ task/crud_with_node/build/precache-manifest.fe2cc1770401deb6ff17a707f34a76e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "28eb7676b0480bba66e4",
+    "url": "/static/js/main.28eb7676.chunk.js"
+  },
+  {
+    "revision": "c33ef99ab9f8dd294d0f",
+    "url": "/static/js/1.c33ef99a.chunk.js"
+  },
+  {
+    "revision": "28eb7676b0480bba66e4",
+    "url": "/static/css/main.8119d9d3.chunk.css"
+  },
+  {
+    "revision": "a764931c4c041b23cd1058c8023463de",
+    "url": "/index.html"
+  }
+];
